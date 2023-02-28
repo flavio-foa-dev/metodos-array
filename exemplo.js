c4a7a23d80ad2b569cb322f2f8f150d19386cf4f
Flavio Andrade <foa12381@gmail.com>
2023-02-28 10:42:32 -0300

    end

--- exemplo.js
+++ exemplo.js
@@ -20,3 +20,54 @@ const arr = ['este é o primeiro elemento', 'este é o segundo elemento', 'este 
 console.log(arr[0]);              // exibe 'este é o primeiro elemento'
 console.log(arr[1]);              // exibe 'este é o segundo elemento'
 console.log(arr[arr.length - 1]); // exibe 'este e o elemento final'
+
+
+const ar= [8,4,7,9,2]
+const result = ar.reduce((acc, current) => {
+  return  acc > current ? current : acc
+})
+console.log(result)
+
+
+function isPalindromo(word){
+  const wordSplit = word.split('')
+  const wordreverse = wordSplit.reverse().join('')
+
+
+  const result = word === wordreverse ? "sim" : "nao"
+ console.log(result)
+}
+
+isPalindromo("rever")
+
+
+const methods = [
+  'join', 'reverse', 'sort', 'push', 'pop', 'shift', 'unshift',
+  'splice', 'concat', 'slice', 'indexOf', 'lastIndexOf',
+  'forEach', 'map', 'reduce', 'reduceRight', 'filter',
+  'some', 'every', 'isArray'
+],
+
+console.log(Array.from(list))
+console.log(Array.isArray(list))
+console.log(Array.of(list))
+
+var board =
+[ ['R','N','B','Q','K','B','N','R'],
+  ['P','P','P','P','P','P','P','P'],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  ['p','p','p','p','p','p','p','p'],
+  ['r','n','b','q','k','b','n','r']];
+console.log(board.join('\n') + '\n\n');
+
+values = [];
+for (var x = 0; x < 10; x++){
+ values.push([
+  2 ** x,
+  2 * x ** 2
+ ])
+};
+console.table(values)
